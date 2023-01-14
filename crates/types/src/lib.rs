1f633bf932c763aa2527e0489ece99c125df87f6
overlookmotel <theoverlookmotel@gmail.com>
2023-01-14 16:22:42 +0000

    More complex types with serde annotations

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,10 +1,34 @@
+use serde::{Deserialize, Serialize};
 use struct_inspect::Inspect;
 
-#[derive(Inspect)]
+#[derive(Inspect, Serialize, Deserialize)]
 pub struct Foo {
     pub small: u8,
     pub big: u32,
+    #[serde(rename = "middle")]
     pub mid: u16,
-    pub arr: Vec<u8>,
+    #[serde(flatten)]
+    pub bar: Bar,
+    pub arr: Vec<Qux>,
+    pub switch: Switch,
     pub recurse: Option<Box<Foo>>,
+}
+
+#[derive(Inspect, Serialize, Deserialize)]
+pub struct Bar {
+    pub smaller: u8,
+}
+
+#[derive(Inspect, Serialize, Deserialize)]
+pub enum Qux {
+    Sun(Foo),
+    Moon(Bar),
+}
+
+#[derive(Inspect, Serialize, Deserialize)]
+pub enum Switch {
+    /// `on`
+    On,
+    /// `off`
+    Off,
 }
